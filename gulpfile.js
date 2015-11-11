1d80680c69a78f53b43ab36c1a3fdc7f20755f1d
David Darnes <me@daviddarnes.com>
2015-11-11 09:44:15 +0000

    Creates demo page with gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var path = require('path');
+
+gulp.task('svgstore', function () {
+    return gulp
+        .src('svg/*.svg')
+        .pipe(svgmin(function (file) {
+            var prefix = path.basename(file.relative, path.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        prefix: prefix + '-',
+                        minify: true
+                    }
+                }]
+            }
+        }))
+        .pipe(svgstore())
+        .pipe(gulp.dest('./'));
+});
