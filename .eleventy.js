d4a672c9fd92f02ca33ae0db984df5052c5d17e2
David Darnes <me@daviddarnes.com>
2021-04-23 16:51:32 +0100

    Add twitter card image

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/png/**.*");
   eleventyConfig.addPassthroughCopy("src/favicon.svg");
   eleventyConfig.addPassthroughCopy("src/background.jpg");
+  eleventyConfig.addPassthroughCopy("src/thumbnail.png");
 
   return {
     dir: {
